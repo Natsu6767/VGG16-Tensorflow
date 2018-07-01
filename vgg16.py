6013bcc3d87199ab20ade3b631d3b7878057b635
Mohit Jain <mohitjain1999@yahoo.com>
2018-07-02 01:23:45 +0400

    Create VGG16 class implementing the network.

--- vgg16.py
+++ vgg16.py
@@ -6,9 +6,52 @@ class VGG16(object):
 
 	""" Implementation of VGG16 network """
 
+	def __init__(self, x, keep_prob, num_classes):
 
-def conv_layer(x, filter_height, filter_width,
-	num_filters, stride, name, padding = 'SAME'):
+		self.X = x
+		self.NUM_CLASSES = num_classes
+		self.KEEP_PROB = keep_prob
+
+		self.create()
+
+	def create(self):
+
+		conv1_1 = conv_layer(self.X, 64, 'conv1_1')
+		conv1_2 = conv_layer(conv1_1, 64, 'conv1_2')
+		pool1 = max_pool(conv1_2, 'pool1')
+
+		conv2_1 = conv_layer(pool1, 128, 'conv2_1')
+		conv2_2 = conv_layer(conv2_1, 128, 'conv2_2')
+		pool2 = max_pool(conv2_2, 'pool2')
+
+		conv3_1 = conv_layer(pool2, 256, 'conv3_1')
+		conv3_2 = conv_layer(conv3_1, 256, 'conv3_2')
+		conv3_3 = conv_layer(conv3_2, 256, 'conv3_3')
+		pool3 = max_pool(conv3_3, 'pool3')
+
+		conv4_1 = conv_layer(pool3, 512, 'conv4_1')
+		conv4_2 = conv_layer(conv4_1, 512, 'conv4_2')
+		conv4_3 = conv_layer(conv4_2, 512, 'conv4_3')
+		pool4 = max_pool(conv4_3, 'pool4')
+
+		conv5_1 = conv_layer(pool4, 512, 'conv5_1')
+		conv5_2 = conv_layer(conv5_1, 512, 'conv5_2')
+		conv5_3 = conv_layer(conv5_2, 512, 'conv5_3')
+		pool5 = max_pool(conv5_3, 'pool5')
+
+		flattened = tf.reshape(pool5, [-1, 1 * 1 * 512])
+		fc6 = fc_layer(flattened, 1 * 1 * 512, 4096, name = 'fc6')
+		dropout6 = dropout(fc6, self.KEEP_PROB)
+
+		fc7 = fc_layer(dropout6, 4096, 4096, name = 'fc7')
+		dropout7 = dropout(fc7, self.KEEP_PROB)
+
+		self.fc8 = fc_layer(dropout7, 4096, self.NUM_CLASSES, relu = False, name = 'fc8')
+
+
+
+def conv_layer(x, num_filters, name, filter_height = 3, filter_width = 3,
+	stride = 1, padding = 'SAME'):
 
 	input_channels = int(x.get_shape()[-1])
 
